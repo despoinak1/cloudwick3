1b9485c3e9e7bf2fd95ba0d359404369eef297a5
Despoina Karna <falsedk@hotmail.com>
2016-03-03 12:03:25 +0000

    Initial commit of project

---
+++ README.md
@@ -0,0 +1,2 @@
+# cloudwick
+cloudwick
---
+++ src/cloudwickProject1/Test1.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package cloudwickProject1;
+
+/**
+ * @author desp
+ *
+ */
+public class Test1 {
+	
+	   public static void main(String[] args) { 
+		   System.out.println("Hello, World");
+	   }
+		
+}
